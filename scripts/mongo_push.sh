f128c08597227a10c22a811d13b5849899a0f90d
Geoffrey Challen <challen@illinois.edu>
2018-08-18 00:30:43 -0500

    Fix push script.

--- scripts/mongo_push.sh
+++ scripts/mongo_push.sh
@@ -8,8 +8,8 @@ mongoexport --uri="$LOCAL" -c state -o fixtures/pushState.json && \
 mongoexport --uri="$LOCAL" -c people -o fixtures/pushPeople.json && \
 mongoexport --uri="$LOCAL" -c peopleChanges -o fixtures/pushPeopleChanges.json && \
 mongoexport --uri="$LOCAL" -c enrollment -o fixtures/pushEnrollment.json && \
-mongoexport --uri="$LOCAL" -c photos -o fixtures/pushPhotos.json
-mongoexport --uri="$LOCAL" -c discourseUsers -o fixtures/pushDiscourseUsers.json
+mongoexport --uri="$LOCAL" -c photos -o fixtures/pushPhotos.json && \
+mongoexport --uri="$LOCAL" -c discourseUsers -o fixtures/pushDiscourseUsers.json &&
 mongoexport --uri="$LOCAL" -c discourseUserChanges -o fixtures/pushDiscourseUserChanges.json
 
 REMOTE="mongodb://cs125:$password@cs125-mongo-01.cs.illinois.edu,cs125-mongo-02.cs.illinois.edu,cs125-mongo-03.cs.illinois.edu/cs125?replicaSet=cs125&ssl=true"
@@ -17,6 +17,6 @@ mongoimport --drop --uri="$REMOTE" -c state fixtures/pushState.json && \
 mongoimport --drop --uri="$REMOTE" -c people fixtures/pushPeople.json && \
 mongoimport --drop --uri="$REMOTE" -c peopleChanges fixtures/pushPeopleChanges.json && \
 mongoimport --drop --uri="$REMOTE" -c enrollment fixtures/pushEnrollment.json && \
-mongoimport --drop --uri="$REMOTE" -c photos fixtures/pushPhotos.json
-mongoimport --drop --uri="$REMOTE" -c discourseUsers -o fixtures/pushDiscourseUsers.json
-mongoimport --drop --uri="$REMOTE" -c discourseUserChanges -o fixtures/pushDiscourseUserChanges.json
+mongoimport --drop --uri="$REMOTE" -c photos fixtures/pushPhotos.json && \
+mongoimport --drop --uri="$REMOTE" -c discourseUsers fixtures/pushDiscourseUsers.json && \
+mongoimport --drop --uri="$REMOTE" -c discourseUserChanges fixtures/pushDiscourseUserChanges.json
